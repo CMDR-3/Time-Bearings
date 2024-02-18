0cfd714e351e822017d2514c578eb2831409cb9b
CMDR-3 <supernovaplayz04@gmail.com>
2024-02-18 12:06:53 -0700

    Blocks and Items, some models and item groups

---
+++ src/main/java/com/zetalasis/timebearings/blocks/TBBlocks.java
@@ -0,0 +1,36 @@
+package com.zetalasis.timebearings.blocks;
+
+import com.zetalasis.timebearings.TimeBearings;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class TBBlocks {
+    public static final Block CLOCKWORK_TABLE = registerBlock("clockwork_table",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
+    public static final Block CLOCKWORK_TARDIS_EXTERIOR = registerBlock("clockwork_tardis_exterior",
+            new Block(FabricBlockSettings.copyOf(Blocks.COPPER_BLOCK)));
+
+    private static Block registerBlock(String name, Block block)
+    {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(TimeBearings.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block)
+    {
+        return Registry.register(Registries.ITEM, new Identifier(TimeBearings.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
+    }
+
+    public static void RegisterBlocks()
+    {
+        TimeBearings.LOGGER.info("Registering blocks");
+    }
+}
---
+++ src/main/java/com/zetalasis/timebearings/items/TBItems.java
@@ -0,0 +1,30 @@
+package com.zetalasis.timebearings.items;
+
+import com.zetalasis.timebearings.TimeBearings;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class TBItems {
+    public static final Item CLOCKWORK_UTILITIES = register("clockwork_utilities", new Item(new FabricItemSettings()));
+
+    private static Item register(String name, Item item)
+    {
+        return Registry.register(Registries.ITEM, new Identifier(TimeBearings.MOD_ID, name), item);
+    };
+
+    private static void ToolsTabInit(FabricItemGroupEntries entries)
+    {
+        entries.add(CLOCKWORK_UTILITIES);
+    }
+
+    public static void RegisterItems()
+    {
+        TimeBearings.LOGGER.info("Setting up items");
+    }
+}
